56dacd8a23027a2031b2709f09df8333760e7b76
Marco Gancitano <mgancita@stevens.edu>
2022-05-31 22:19:20 -0400

    Move to `static` target =

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,8 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  target: 'static',
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
